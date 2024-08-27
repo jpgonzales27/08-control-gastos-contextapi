9b942e9732efa86de0ecd9afea07e00730583624
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-08-27 10:20:16 -0400

    Creado BudgetContext y y usando useContext

---
+++ src/context/BudgetContext.tsx
@@ -0,0 +1,27 @@
+import { createContext, Dispatch, useReducer } from "react";
+import { BudgetActions, budgetReducer, BudgetState, initialState } from "../reducers/budget-reducer";
+
+type BudgetContextProps = {
+  state: BudgetState;
+  dispatch: Dispatch<BudgetActions>;
+};
+
+type BudgetProviderProps = {
+  children: React.ReactNode;
+};
+
+export const BudgetContext = createContext<BudgetContextProps>(null!);
+
+export const BudgetProvider = ({ children }: BudgetProviderProps) => {
+  const [state, dispatch] = useReducer(budgetReducer, initialState);
+  return (
+    <BudgetContext.Provider
+      value={{
+        state,
+        dispatch,
+      }}
+    >
+      {children}
+    </BudgetContext.Provider>
+  );
+};
