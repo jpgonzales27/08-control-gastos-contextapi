affb82f5aed94c572d8c5b8b76ace34116141e4b
Juan Pablo Gonzales <juampi7237@gmail.com>
2024-08-31 19:13:24 -0400

    Creado formulario para agregar gasto

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,3 +1,68 @@
+import { categories } from "../data/categories";
+import DatePicker from "react-date-picker";
+import "react-calendar/dist/Calendar.css";
+import "react-date-picker/dist/DatePicker.css";
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
 export default function ExpenseForm() {
-  return <div>ExpenseForm</div>;
+  return (
+    <form className="space-y-5">
+      <legend>Nuevo Gasto</legend>
+      <div className="flex flex-col gap-2">
+        <label htmlFor="expenseName" className="text-xl">
+          Nombre Gasto
+        </label>
+        <input
+          type="text"
+          name="expenseName"
+          id="expenseName"
+          placeholder="Añade el nombre del gasto"
+          className="bg-slate-100 p-2"
+        />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="amount" className="text-xl">
+          Cantidad
+        </label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          placeholder="Añade la cantidad del gasto: ej. 300"
+          className="bg-slate-100 p-2"
+        />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="category" className="text-xl">
+          Categoria
+        </label>
+        <select name="category" id="category" className="bg-slate-100 p-2">
+          <option value="">----Selecciona una categoria----</option>
+          {categories.map((category) => (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="amount" className="text-xl">
+          Fecha Gasto:
+        </label>
+        <DatePicker className="bg-slate-100 p-2 border-0" />
+      </div>
+
+      <input
+        type="submit"
+        className="bg-blue-600 cursor-pointer w-full text-white uppercase font-bold rounded-lg p-2"
+        value={"Regitrar Gasto"}
+      />
+    </form>
+  );
 }
